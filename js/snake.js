06d579a84e85d7b84e38df069870ac210bb2040e
harikaharish <harikabadam69@gmail.com>
2015-06-25 14:40:43 -0700

    Added backgound grid, keycode defect fix,changed background color

--- js/snake.js
+++ js/snake.js
@@ -2,7 +2,7 @@ function init() {
 
     var width = 65;
     var height = 40;
-
+    var ctx1;
     var ctx;
     var turn = [];
 
@@ -32,16 +32,46 @@ function init() {
     var win = window;
     var doc = document;
     var canvas = doc.createElement('canvas');
+    var canvas1 = doc.createElement('canvas');
     var setInt = win.setInterval;
     var clInt = win.clearInterval;
     for (i = 0; i < width; i++) {
         map[i] = [];
     }
-
+    // canvas for snake and food
     canvas.setAttribute('width', width * 10);
     canvas.setAttribute('height', height * 10);
     ctx = canvas.getContext('2d');
     doc.body.appendChild(canvas);
+
+    // first canvas for background grid
+    canvas1.setAttribute('width', width * 10);
+    canvas1.setAttribute('height', height * 10);
+    ctx1 = canvas1.getContext('2d');
+    doc.body.appendChild(canvas1);
+
+    //vertical lines
+
+    for (var i = 0; i < width; i++){
+        ctx1.beginPath();
+        ctx1.moveTo(i * 10 , 0 );
+        ctx1.lineTo( i * 10, height * 10 );
+        //set line color
+        ctx1.strokeStyle = '#8C9191';
+        ctx1.stroke();
+
+        }
+
+     //Horizontal lines
+
+    for (var y = 0; y < height; y++){
+
+        ctx1.beginPath();
+        ctx1.moveTo(0 , y *10 );
+        ctx1.lineTo( width * 10 ,y * 10 );
+        ctx1.stroke();
+
+        }
     function placeFood() {
         var x, y;
         do {
@@ -50,10 +80,20 @@ function init() {
         } while (map[x][y]);
         map[x][y] = 1;
         ctx.strokeRect(x * 10 + 1, y * 10 + 1, 10 - 2, 10 - 2);
+        ctx.strokeStyle = "green";
     }
 
     placeFood();
     placeFood();
+
+    function rollCredits(){
+        ctx.fillStyle = '#f00';
+        ctx.font = 'italic bold 30px sans-serif';
+        ctx.textBaseline = 'Middle';
+        ctx.fillText('GAME OVER!!!', 220, 100);
+        ctx.fillText(lastMovee + "- you lost buddy :(", 150,150);
+    }
+
     function restart() {
         ctx.clearRect(0, 0, width * 10, height * 10);
         map = [];
@@ -89,13 +129,14 @@ function init() {
 
         if ((easy || (0 <= snake1.X && 0 <= snake1.Y && snake1.X < width && snake1.Y < height)) && 2 !== map[snake1.X][snake1.Y]) {
             if (1 === map[snake1.X][snake1.Y]) {
-                score += Math.max(5, inc_score);
+                snake1.score += Math.max(5, inc_score);
                 inc_score = 50;
                 placeFood();
                 snake1.elements++;
             }
 
             ctx.fillRect(snake1.X * 10, snake1.Y * 10, 10 - 1, 10 - 1);
+            ctx.fillStyle = "#E6730E";
             map[snake1.X][snake1.Y] = 2;
             snake1.tail.unshift([snake1.X, snake1.Y]);
 
@@ -110,7 +151,7 @@ function init() {
             }
 
         } else if (!turn.length) {
-            restart();
+            rollCredits();
         }
         if ((easy || (0 <= snake2.X && 0 <= snake2.Y && snake2.X < width && snake2.Y < height)) && 2 !== map[snake2.X][snake2.Y]) {
             if (1 === map[snake2.X][snake2.Y]) {
@@ -121,6 +162,7 @@ function init() {
             }
 
             ctx.fillRect(snake2.X * 10, snake2.Y * 10, 10 - 1, 10 - 1);
+            ctx.fillStyle = "#9E0EE6";
             map[snake2.X][snake2.Y] = 2;
             snake2.tail.unshift([snake2.X, snake2.Y]);
 
@@ -135,12 +177,15 @@ function init() {
             }
 
         } else if (!turn.length) {
-            restart();
+            rollCredits();
+            
         }
     }
 
     interval = setInt(clock, 120);
+    var lastMovee;
 
+    // Adding keyboard controls
     doc.onkeydown = function (e) {
 
         var code = e.keyCode - 37;
@@ -151,17 +196,34 @@ function init() {
          * 2: right
          * 3: down
          **/
-        if (0 <= code && code < 4 && code !== turn[0]) {
-            //turn.unshift(code);
-            snake1.direction = code;
-        } else if(e.keyCode == 65) {
+         //first snake arrow keys
+        if (code == 0 && snake1.direction !=2){
+            snake1.direction=0;
+            lastMovee = "Player1";
+        }else if (code == 1 && snake1.direction !=3){
+            snake1.direction=1;
+            lastMovee = "Player1";
+
+        }else if (code == 2 && snake1.direction !=0){
+            snake1.direction=2;
+            lastMovee = "Player1";
+
+        }else if (code == 3 && snake1.direction !=1){
+            snake1.direction=3;
+            lastMovee = "Player1";
+
+        }else if(e.keyCode == 65 && snake2.direction!=2) { //second snake wasd keys
             snake2.direction = 0;
-        } else if(e.keyCode == 87) {
+            lastMovee = "Player 2";
+        } else if(e.keyCode == 87 && snake2.direction != 3) {
             snake2.direction = 1;
-        } else if(e.keyCode == 68) {
+            lastMovee = "Player 2";
+        } else if(e.keyCode == 68 && snake2.direction !=0) {
             snake2.direction = 2;
-        } else if(e.keyCode == 83) {
+            lastMovee = "Player 2";
+        } else if(e.keyCode == 83 && snake2.direction != 1) {
             snake2.direction = 3;
+            lastMovee = "Player 2";
         }
     }
 }
